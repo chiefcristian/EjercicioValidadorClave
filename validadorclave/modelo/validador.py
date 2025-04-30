83962c37405455320a8a29834bacc5b1c53c28d0
miguel <turutaperfecta@gmail.com>
2025-04-30 12:48:21 -0500

    crear clase de las regla para la validacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,24 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada: int):
+        self._longitud_esperada = longitud_esperada
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+    def _validar_longitud(self, clave: str) -> bool:
+        return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave: str) -> bool:
+        return any(c.isdigit() for c in clave)
+
+
