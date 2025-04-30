c1d580baa6d2c39ee5c8c99c1f6f00629f37bbbb
miguel <turutaperfecta@gmail.com>
2025-04-30 12:55:04 -0500

    crear clase reglaValidacionGanimede Verifica en orden: longitud > 8, mayúscula, minúscula, número y caracter especial.

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -22,3 +22,30 @@ class ReglaValidacion(ABC):
         return any(c.isdigit() for c in clave)
 
 
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        super().__init__(longitud_esperada=8)
+
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        caracteres_especiales = {'@', '_', '#', '$', '%'}
+        return any(c in caracteres_especiales for c in clave)
+
+    def es_valida(self, clave: str) -> bool:
+        if not self._validar_longitud(clave):
+            raise ValueError("La clave debe tener una longitud de más de 8 caracteres")
+
+        if not self._contiene_mayuscula(clave):
+            raise ValueError("La clave debe tener al menos una letra mayúscula")
+
+        if not self._contiene_minuscula(clave):
+            raise ValueError("La clave debe tener al menos una letra minúscula")
+
+        if not self._contiene_numero(clave):
+            raise ValueError("La clave debe tener al menos un número")
+
+        if not self.contiene_caracter_especial(clave):
+            raise ValueError("La clave debe tener al menos un caracter especial (@, _, #, $, %)")
+
+        return True
+
+
